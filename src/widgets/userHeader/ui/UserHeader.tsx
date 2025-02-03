ffe5a67ddca325e683837a279defd4a1fa882869
Andrey Valiuk <andrey.valyuk.04@mail.ru>
2025-02-03 16:59:25 +0300

    fix css

--- src/widgets/userHeader/ui/UserHeader.tsx
+++ src/widgets/userHeader/ui/UserHeader.tsx
@@ -8,20 +8,18 @@ export const UserHeader = () => {
   const navigate = useNavigate();
   return (
     <div className={styles.header}>
-      <div className="container">
-        <div className={styles.header__container}>
-          <img
-            src={arrow}
-            alt="arrow"
-            className={styles.user__arrow}
-            onClick={() => navigate(-1)}
-          />
-          <div className={styles.user}>
-            <img src={user} alt="user" />
-            <div className={styles.inf}>
-              <h3 className={styles.title}>Ян Я.</h3>
-              <p className={styles.text}>в сети </p>
-            </div>
+      <div className={styles.header__container}>
+        <img
+          src={arrow}
+          alt="arrow"
+          className={styles.user__arrow}
+          onClick={() => navigate(-1)}
+        />
+        <div className={styles.user}>
+          <img src={user} alt="user" />
+          <div className={styles.inf}>
+            <h3 className={styles.title}>Ян Я.</h3>
+            <p className={styles.text}>в сети </p>
           </div>
         </div>
       </div>
